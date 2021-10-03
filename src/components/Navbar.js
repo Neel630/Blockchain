78f7ce33542d92b32f3cd25e61f18b8ae201d521
Neel <53505500+Neel630@users.noreply.github.com>
2021-10-03 17:07:11 +0530

    Navbar css changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,36 @@
-import React, { Component } from 'react'
-import farmer from '../farmer.png'
+import React from "react";
+import stake from "../stake.png";
 
-class Navbar extends Component {
-
-  render() {
-    return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+const Navbar = ({ account }) => {
+  return (
+    <div className="container-fluid pb-5">
+      <nav className="navbar navbar-light p-3 fixed-top bg-light flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
+          href="https://github.com/Neel630/defi-dapp"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src={farmer} width="30" height="30" className="d-inline-block align-top" alt="" />
+          <img
+            src={stake}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt=""
+          />
           &nbsp; DApp Token Farm
         </a>
 
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <small className="text-secondary">
-              <small id="account">{this.props.account}</small>
+              <small id="account">{account}</small>
             </small>
           </li>
         </ul>
       </nav>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Navbar;
