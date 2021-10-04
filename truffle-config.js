a56564e681db0d09a892ca06f2e31666077fb073
Neel <53505500+Neel630@users.noreply.github.com>
2021-10-04 22:39:18 +0530

    rinkiby network

--- truffle-config.js
+++ truffle-config.js
@@ -1,23 +1,34 @@
-require('babel-register');
-require('babel-polyfill');
+require("babel-register");
+require("babel-polyfill");
+// const HDWalletProvider = require("@truffle/hdwallet-provider");
+// const mnemonic = "";
+// const rinkibyInfuraURL = "";
 
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
     },
+    // rinkeby: {
+    //   provider: () => new HDWalletProvider(mnemonic, rinkibyInfuraURL),
+    //   network_id: 4,
+    //   gas: 5500000,
+    //   confirmations: 2,
+    //   timeoutBlocks: 200,
+    //   skipDryRun: true,
+    // },
   },
-  contracts_directory: './src/contracts/',
-  contracts_build_directory: './src/abis/',
+  contracts_directory: "./src/contracts/",
+  contracts_build_directory: "./src/abis/",
   compilers: {
     solc: {
       optimizer: {
         enabled: true,
-        runs: 200
+        runs: 200,
       },
-      evmVersion: "petersburg"
-    }
-  }
-}
+      evmVersion: "petersburg",
+    },
+  },
+};
